b651eca416658f4d013595f23b4d650298daf63c
Rawiphon-27990 <27990@ud.ac.th>
2026-09-16 11:34:22 +0700

    Update Examination_5.py

--- Examination_5.py
+++ Examination_5.py
@@ -7,4 +7,10 @@
 
 
 # นักเรียนเขียนโค้ดต่อจากบรรทัดนี้
-age = int(input())
+age = int(input("กรอกอายุ:"))
+if age < 12:
+    print("จ่าย50")
+elif age < 59:
+    print("จ่าย100")   
+else:
+    print("เข้าฟรี")    
